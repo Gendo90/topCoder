afa91562a37b177a6c5366f2a827b81396e1b6ec
Patrick Gendotti <patrick.gendotti@gmail.com>
2019-05-03 13:26:17 -0700

    feat: Add "Guess The Number" solution to repo - essentially a binary search algorithm that just counts guesses

---
+++ 0-300 pts/topcoderGuessTheNumber.py
@@ -0,0 +1,50 @@
+#Problem Statement
+#    
+#A popular guessing game is "Guess the number", where one person selects a number in a known range, and another person tries to guess that number. After each guess, the first person reveals whether the guess was correct, too high or too low.
+#Pretty soon one learns the best strategy, which is to guess the middle number among those not yet ruled out. If there is no single middle number, then there are two numbers to choose from. In that case, we choose the smallest of those numbers.
+#The algorithm can be described like this:
+#Init lower and upper bound
+#Repeat
+#  x = (lower bound + upper bound)/2  (round down if necessary)
+#  Make guess x
+#  If x is too low, set lower bound to x+1
+#  If x is too high, set upper bound to x-1
+#Until x is correct
+#For instance, assume that the lower and upper bound initally are 1 and 9, respectively. The middle number in this range is 5. If this is "too low", the new bounds become 6 and 9. This range contains four numbers, and there is thus no single middle number. The two numbers in the middle are 7 and 8, and the smallest of these is 7, so our next guess then becomes 7.
+#Create a class GuessTheNumber which contains the method noGuesses which takes an integer upper, the initial upper bound of the range (the inital lower bound is always 1), and an integer answer, the number selected by the first person. The method should return an integer representing the total number of guesses required for the second person to guess the correct number using the method described above.
+#Definition
+#    
+#Class:
+#GuessTheNumber
+#Method:
+#noGuesses
+#Parameters:
+#integer, integer
+#Returns:
+#integer
+#Method signature:
+#def noGuesses(self, upper, answer):
+
+#Gendo90 has submitted the 250-point problem for 222.07 points
+#Successful on first try!
+
+class GuessTheNumber(object):
+    def noGuesses(self, upper, answer):
+        count = 1
+        lower = 1
+        guess = (upper+lower)//2
+        while(guess!=answer):
+            count += 1
+            if(guess<answer):
+                lower = guess+1
+            else:
+                upper = guess-1
+            guess = (upper+lower)//2
+
+        return count
+
+
+
+test = GuessTheNumber()
+
+print(test.noGuesses(9, 6))
